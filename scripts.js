3c994c8d84ce85fff8b6f24b279c902f9226f9a6
Thaiony <thaiony@hotmail.com>
2025-02-18 01:54:40 -0300

    novos cat

--- scripts.js
+++ scripts.js
@@ -3,7 +3,26 @@ document.addEventListener('DOMContentLoaded', function() {
     
     movies.forEach(movie => {
         movie.addEventListener('click', function() {
-            alert(`Você clicou em---: ${movie.querySelector('h2').innerText}`);
+            alert(`Você clicou em: ${movie.querySelector('h2').innerText}`);
         });
     });
 });
+
+function saibaMais(titulo) {
+    alert('Mais informações sobre: ' + titulo);
+}
+
+function filterMovies(classe) {
+    var movies = document.querySelectorAll('.movie');
+    movies.forEach(function(movie) {
+        if (classe === 'todos') {
+            movie.style.display = 'block';
+        } else {
+            if (movie.classList.contains(classe)) {
+                movie.style.display = 'block';
+            } else {
+                movie.style.display = 'none';
+            }
+        }
+    });
+}
